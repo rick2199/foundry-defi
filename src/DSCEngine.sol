608dd43f371302834fad88c04651231480d89fe1
Ricardo Rojas <richardrojasa@gmail.com>
2024-11-02 22:20:23 -0500

    fix: :adhesive_bandage: fix openzeppelin imports

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -3,8 +3,8 @@
 pragma solidity ^0.8.18;
 
 import {DecentralizedStableCoin} from "src/DecentralizedStableCoin.sol";
-import {ReentrancyGuard} from "lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
-import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {AggregatorV3Interface} from
     "lib/chainlink-brownie-contracts/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import {OracleLib} from "src/libraries/OracleLib.sol";
